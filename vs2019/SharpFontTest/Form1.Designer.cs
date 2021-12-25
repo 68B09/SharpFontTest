88f2c490c9c2bbbbbde9e7766c5178b92f674b1c
68B09 <zzo@pullup.net>
2021-12-25 15:07:31 +0900

    1st

---
+++ vs2019/SharpFontTest/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace SharpFontTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblBad = new System.Windows.Forms.Label();
+			this.lblBaseLine = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lblBad
+			// 
+			this.lblBad.AutoSize = true;
+			this.lblBad.Location = new System.Drawing.Point(12, 9);
+			this.lblBad.Name = "lblBad";
+			this.lblBad.Size = new System.Drawing.Size(345, 12);
+			this.lblBad.TabIndex = 0;
+			this.lblBad.Text = "ベースラインを計算しないとカンマやアンダーラインなどが悲惨なことになる。";
+			// 
+			// lblBaseLine
+			// 
+			this.lblBaseLine.AutoSize = true;
+			this.lblBaseLine.Location = new System.Drawing.Point(12, 121);
+			this.lblBaseLine.Name = "lblBaseLine";
+			this.lblBaseLine.Size = new System.Drawing.Size(93, 12);
+			this.lblBaseLine.TabIndex = 1;
+			this.lblBaseLine.Text = "ベースラインを計算";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.lblBaseLine);
+			this.Controls.Add(this.lblBad);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblBad;
+		private System.Windows.Forms.Label lblBaseLine;
+	}
+}
+
---
+++ vs2019/SharpFontTest/Form1.cs
@@ -0,0 +1,109 @@
+﻿using SharpFont;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SharpFontTest
+{
+	public partial class Form1 : Form
+	{
+		private Library sfLib;
+		private Face sfFace;
+		private Pen penBlue = new Pen(Color.Blue);
+		private Pen penRed = new Pen(Color.Red);
+		private Pen penGray = new Pen(Color.Gray);
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			this.sfLib = new Library();
+
+			// 3つ目の引数はファイル内インデックス。
+			// MSGOTHIC.TTCには3つのフォントが入っていて、0番目は「ＭＳ ゴシック」。
+			// .ttfや.otfの場合はフォントが一つしか入っていないので0を指定する。
+			// 使用済みになったFaceはDispose()すること。
+			this.sfFace = new Face(this.sfLib
+								 , @"C:\WINDOWS\Fonts\MSGOTHIC.TTC"
+								 , 0);
+
+			this.sfFace.SelectCharmap(SharpFont.Encoding.Unicode);   // UNICODEマップを使う
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			if (this.DesignMode) {
+				return;
+			}
+
+			string text = @"abcdefghijklmnopqrstuvwxyz,._あ。、";
+			int fontSize = 48;
+
+			// SetPixelSizes()を呼び出してからGetCharIndex()などを呼ばないとエラーになる
+			this.sfFace.SetPixelSizes(0, (uint)fontSize);
+
+			this.Draw1(e.Graphics, this.lblBad.Location.X, this.lblBad.Location.Y + this.lblBad.Height, text, fontSize);
+			this.Draw2(e.Graphics, this.lblBaseLine.Location.X, this.lblBaseLine.Location.Y + this.lblBaseLine.Height, text, fontSize);
+		}
+
+		// ベースラインを考えない
+		private void Draw1(Graphics g, int x, int y, string text, int fontSize)
+		{
+			foreach (char code in text) {
+				uint charIndex = this.sfFace.GetCharIndex(code);
+				this.sfFace.LoadGlyph(charIndex, LoadFlags.Color | LoadFlags.Render, LoadTarget.Normal);
+
+				this.sfFace.Glyph.RenderGlyph(RenderMode.Mono);
+
+				if (this.sfFace.Glyph.Bitmap.Width > 0) {
+					using (Bitmap bitmap = this.sfFace.Glyph.Bitmap.ToGdipBitmap(Color.Black)) {
+						g.DrawImage(bitmap, x, y);
+
+						x += bitmap.Width;
+					}
+				}
+			}
+
+			g.DrawLine(this.penBlue, 0, y, 1000, y);
+			g.DrawLine(this.penRed, 0, y + fontSize, 1000, y + fontSize);
+		}
+
+		// ベースラインを計算する
+		private void Draw2(Graphics g, int x, int y, string text, int fontSize)
+		{
+			// 文字高とディセンダ高からディセンダ率を算出。
+			// 次にフォントサイズ×ディセンダ率でディセンダのピクセルサイズを算出。
+			// 率を求める理由はFace.DescenderとFace.Heightが(おそらく)仮想サイズであるため。
+			// 例えばFace.HeightはSetPixelSizes()で設定した値ではなく"256"だったりする。
+			// https://freetype.org/freetype2/docs/tutorial/step2.html
+			int descender = (int)(fontSize * ((double)-this.sfFace.Descender / (double)this.sfFace.Height));
+
+			foreach (char code in text) {
+				// コードポイントからインデックスを取得
+				uint charIndex = this.sfFace.GetCharIndex(code);
+				// インデックスからグリフ情報を読み込む
+				this.sfFace.LoadGlyph(charIndex, LoadFlags.Color | LoadFlags.Render, LoadTarget.Normal);
+				// グリフをレンダリング
+				this.sfFace.Glyph.RenderGlyph(RenderMode.Mono);
+
+				if (this.sfFace.Glyph.Bitmap.Width > 0) {
+					// Bitmapに変換したレンダリング結果を取得
+					using (Bitmap bitmap = this.sfFace.Glyph.Bitmap.ToGdipBitmap(Color.Black)) {
+						// y + fontSize = 底辺座標。
+						// - descender = 底辺からディセンダを引いてベースライン座標を求める。
+						// - HorizontalBearingY = ベースラインから上部の高さを引いて描画座標を求める。
+						// この計算で良いかどうかは謎。
+						int yy = (int)(y + fontSize - descender - this.sfFace.Glyph.Metrics.HorizontalBearingY);
+						g.DrawImage(bitmap, x, yy);
+
+						x += bitmap.Width;
+					}
+				}
+			}
+
+			g.DrawLine(this.penBlue, 0, y, 1000, y);
+			g.DrawLine(this.penRed, 0, y + fontSize, 1000, y + fontSize);
+			g.DrawLine(this.penGray, 0, y + fontSize - descender, 1000, y + fontSize - descender);
+		}
+	}
+}
